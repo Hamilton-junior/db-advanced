eb8002bf490930d11b2cf9f6ecf38143134ee124
hamilton <hamilton-souza@outlook.com.br>
2022-11-02 11:14:44 -0300

    removed bank acess in class 2 exercises

--- aula-2/exercicio-tres.js
+++ aula-2/exercicio-tres.js
@@ -1,17 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC9zQ95DVy-XHBfaDNEPH77scarXZKDCso",
-  authDomain: "banco-de-dados-avancado.firebaseapp.com",
-  databaseURL: "https://banco-de-dados-avancado-default-rtdb.firebaseio.com",
-  projectId: "banco-de-dados-avancado",
-  storageBucket: "banco-de-dados-avancado.appspot.com",
-  messagingSenderId: "105517626074",
-  appId: "1:105517626074:web:b5def33b0999658b59ebe3",
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
